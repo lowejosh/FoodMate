ab376ab45caeecc9bd47506d98f8e1e97e32eeda
Your name <lowejosh50@gmail.com>
2019-09-09 17:26:28 +1000

    signup page and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import 'typeface-roboto';
 import './App.css';
-import SignIn from './components/SignIn/SignIn';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import SignUp from './components/Pages/SignUp/SignUp';
+import SignIn from './components/Pages/SignIn/SignIn';
 
 const theme = createMuiTheme({
   palette: {
@@ -21,7 +21,6 @@ const App = () => {
         <Route exact path="/signin" component={SignIn} />
         <Route exact path="/signup" component={SignUp} />
         <Route exact path="/" component={SignIn} />
-
       </Router>
     </ThemeProvider>
   );
