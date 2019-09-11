10e2d18fcadf24362f6e45ba5bfe70c1d72f5f62
Your name <lowejosh50@gmail.com>
2019-09-11 17:14:44 +1000

    added cuisine latlng endpoint

--- server/index.js
+++ server/index.js
@@ -1,8 +1,28 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
+const zomato = require('zomato');
 const server = express();
 const PORT = process.env.PORT || 8000;
+const client = zomato.createClient({
+    userKey: process.env.ZOMATO_API_KEY
+});
 
 server.listen(PORT, () => {
     console.log(`Server Running on port: ${PORT}`);
+});
+
+server.get("/cuisines/:lat/:lng", (req, res) => {
+    const lat = req.params.lat;
+    const lng = req.params.lng;
+    client.getCuisines({
+        lat: lat,
+        lon: lng,
+    }, (err, result) => {
+        if (!err) {
+            res.json(result);
+        } else {
+            res.json({ message: `City at given location doesn't exist` })
+            console.log(err);
+        }
+    });
 });
